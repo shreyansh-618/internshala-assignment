7b5180b2cb4cd965f781c092f3a0dd7fbfbfb996
shreyansh-618 <chaudharyshreyash618@gmail.com>
2025-09-10 22:03:32 +0530

    Fixed 404 error on vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
-// App.jsx
-import { Routes, Route, HashRouter as Router } from "react-router-dom"; // <-- changed
+import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { CartProvider } from "./contexts/CartContext";
 import Header from "./components/Header";
@@ -12,33 +11,29 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <Router>
-      {" "}
-      {/* <-- wrap everything in HashRouter */}
-      <AuthProvider>
-        <CartProvider>
-          <div className="App">
-            <Header />
-            <main>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/products" element={<Products />} />
-                <Route
-                  path="/cart"
-                  element={
-                    <ProtectedRoute>
-                      <Cart />
-                    </ProtectedRoute>
-                  }
-                />
-              </Routes>
-            </main>
-          </div>
-        </CartProvider>
-      </AuthProvider>
-    </Router>
+    <AuthProvider>
+      <CartProvider>
+        <div className="App">
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/products" element={<Products />} />
+              <Route
+                path="/cart"
+                element={
+                  <ProtectedRoute>
+                    <Cart />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </main>
+        </div>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
