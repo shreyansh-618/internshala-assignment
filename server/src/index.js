4e5b123755dd49aa80f96d893dc29b9249b5a463
shreyansh-618 <chaudharyshreyash618@gmail.com>
2025-09-10 21:22:58 +0530

    Fix CORS for deployed frontend.

--- server/src/index.js
+++ server/src/index.js
@@ -28,8 +28,9 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://127.0.0.1:3000",
-      "https://internshala-assignment-r2lh.vercel.app/",
+      "https://internshala-assignment-r2lh.vercel.app",
     ],
+
     allowHeaders: ["Content-Type", "Authorization"],
     allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
